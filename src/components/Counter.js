e99d00c069f35d06dcdd0bbd017925e0c2554770
Sati <rock_sal@mail.ru>
2022-09-06 16:24:27 +0600

    Connecting React and Redux with connect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,55 @@
-const Counter = ({counter, inc, dec, rnd}) => {
+import { connect } from "react-redux";
+// import { bindActionCreators } from "redux";
 
-    return (
+import * as actions from '../actions';
 
-        <div className="jumbotron">
-            <h1>{counter}</h1>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
-        </div>
-    );
+// в классовом компоненте тоже будет работать
+
+import { Component } from "react";
+
+class Counter extends Component {
+
+    render() {
+
+        const {counter, inc, dec, rnd} = this.props;
+
+        return (
+
+            <div className="jumbotron">
+                <h1>{counter}</h1>
+                <button onClick={inc} className="btn btn-primary">INC</button>
+                <button onClick={dec} className="btn btn-primary">DEC</button>
+                <button onClick={rnd} className="btn btn-primary">RND</button>
+            </div>
+        );
+    }
 }
 
-export default Counter;
+// const Counter = ({counter, inc, dec, rnd}) => {
+
+//     return (
+
+//         <div className="jumbotron">
+//             <h1>{counter}</h1>
+//             <button onClick={inc} className="btn btn-primary">INC</button>
+//             <button onClick={dec} className="btn btn-primary">DEC</button>
+//             <button onClick={rnd} className="btn btn-primary">RND</button>
+//         </div>
+//     );
+// }
+
+const mapStateToProps = (state) => {
+
+    return {
+
+        counter: state.value
+    }
+} 
+
+// вместо этой функции можем прописать просто actions
+// const mapDispatchToProps = (dispatch) => {
+
+//     return bindActionCreators(actions, dispatch)
+// }
+
+export default connect(mapStateToProps, actions)(Counter);
